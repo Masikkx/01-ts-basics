729e00ebac8d2d12ab84988b0558fc933e5feeb3
Masikkx <www.macs2009@gmail.com>
2025-11-23 18:09:09 +0200

    Fixed errors

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,7 +2,7 @@ function getFirstElement1<T>(arr: T[]):T {
   return arr[0];
 }
 
-getFirstElement1([1, 2, 3]);           // 1
-getFirstElement1(["a", "b", "c"]);     // "a"
-getFirstElement1([true, false, true]); // true
+getFirstElement1<number>([1, 2, 3]);           // 1
+getFirstElement1<string>(["a", "b", "c"]);     // "a"
+getFirstElement1<boolean>([true, false, true]); // true
 
